e24ee5c3a803ef9cc63f866ac1664b7a1fa1daa1
Melody <137832068+Melody-Lisa@users.noreply.github.com>
2024-10-18 12:35:16 +0000

    Replace checkout and error handler alerts with modals

--- assets/script/practice_match.js
+++ assets/script/practice_match.js
@@ -2,6 +2,12 @@ let initialScore = 501;
 let numbersEntered = [];
 let scoreHistory = [initialScore]; // Track score history, starting with the initial score
 
+// Initialise modals
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+});
+
 
 function startGame() {
     initialScore = 501;
@@ -43,26 +49,36 @@ function deleteLastNumber() {
     }
 }
 
+// Function to clear the input area //
+function clearInput() {
+    numbersEntered.length = 0;
+    displayConcatenatedNumbers();
+}
 
 // Function to update the score
 function updateScore() {
     const concatenatedNumber = parseInt(numbersEntered.join(''));
-    
+
     if (isNaN(concatenatedNumber)) {
-        alert('Please enter a valid number');
+        // Error handler if anything other a number is entered
+        var modalInstance = M.Modal.getInstance(document.getElementById('nanModal'));
+        modalInstance.open();
         return;
     }
 
     if (concatenatedNumber > 180) {
-        // If the entered number exceeds 180, show an error and don't update the score
-        alert('Invalid input: A single score entry cannot exceed 180!');
+        // Error handler if the entered number exceeds 180
+        var modalInstance = M.Modal.getInstance(document.getElementById('tooHighModal'));
+        modalInstance.open();
         return;
     }
 
     const newScore = initialScore - concatenatedNumber;
 
     if (newScore < 0) {
-        alert('Invalid input: Score cannot go below 0');
+        // Error handler if the entered number drops the score below 0
+        var modalInstance = M.Modal.getInstance(document.getElementById('minusModal'));
+        modalInstance.open();
     } else {
         scoreHistory.push(newScore);  // Save the current score before updating
         initialScore = newScore;      // Update the score
@@ -73,7 +89,8 @@ function updateScore() {
 
     if (newScore == 0) {
         updateScoreDisplay();
-        alert('You checked out!');
+        var modalInstance = M.Modal.getInstance(document.getElementById('checkoutModal'));
+        modalInstance.open();
     }
 }
 
